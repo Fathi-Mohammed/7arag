8d28c06ad8d3d8b730c11ac1e491fc320e24d88c
Fathi <fathymohammed2229@gmail.com>
2023-08-15 22:13:42 +0300

    upload site files

---
+++ assets/js/plugin.js
@@ -0,0 +1,346 @@
+jQuery(document).ready(function ($) {
+  lazyLoad();
+
+  const introSlider = swiperInit({
+    className: ".intro_slider_wrapper",
+    breakpoints: false,
+    observer: true,
+    observeParents: true,
+  });
+
+  let bestSellerSlider = swiperInit({
+    className: ".best_seller_slider_wrapper",
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      480: {
+        slidesPerView: 2,
+      },
+      767: {
+        slidesPerView: 3,
+      },
+      992: {
+        slidesPerView: 4,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+    observer: true,
+    observeParents: true,
+    grid: {
+      row: 2,
+    },
+  });
+
+  const ourChoiceSlider = swiperInit({
+    className: ".our_choice_wrapper",
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      480: {
+        slidesPerView: 1,
+      },
+      767: {
+        slidesPerView: 1,
+      },
+      992: {
+        slidesPerView: 2,
+      },
+      1200: {
+        slidesPerView: 2,
+      },
+    },
+    observer: true,
+    observeParents: true,
+    grid: {
+      row: 2,
+    },
+  });
+
+  showPassword($);
+  collapseFooterMenusInSmallScreens($);
+  toggleSideMenuInSmallScreens($);
+  stickyHeader($);
+  verificationCodeSeprate();
+  selectPIckerInit($);
+
+  changeProductCardShow();
+
+  // dragFilesUpload();
+});
+
+// functions init
+function selectPIckerInit($) {
+  $(".selectpicker").selectpicker();
+}
+
+function lazyLoad() {
+  const images = document.querySelectorAll(".lazy-omd");
+
+  const optionsLazyLoad = {
+    //  rootMargin: '-50px',
+    // threshold: 1
+  };
+
+  const preloadImage = function (img) {
+    img.src = img.getAttribute("data-src");
+    img.onload = function () {
+      img.parentElement.classList.remove("loading-omd");
+      img.parentElement.classList.add("loaded-omd");
+      img.parentElement.parentElement.classList.add("lazy-head-om");
+    };
+  };
+
+  const imageObserver = new IntersectionObserver(function (enteries) {
+    enteries.forEach(function (entery) {
+      if (!entery.isIntersecting) {
+        return;
+      } else {
+        preloadImage(entery.target);
+        imageObserver.unobserve(entery.target);
+      }
+    });
+  }, optionsLazyLoad);
+
+  images.forEach(function (image) {
+    imageObserver.observe(image);
+  });
+}
+
+function swiperInit(
+  options = {
+    className: "",
+    breakpoints: null,
+    observer: false,
+    observeParents: false,
+  }
+) {
+  return new Swiper(options.className + " .swiper-container", {
+    spaceBetween: 30,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: options.className + " .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: options.className + " .swiper-button-next",
+      prevEl: options.className + " .swiper-button-prev",
+    },
+    breakpoints: options.breakpoints,
+    observer: options.observer,
+    observeParents: options.observeParents,
+  });
+}
+
+function verificationCodeSeprate() {
+  const inputElements = [...document.querySelectorAll("input.code-input")];
+
+  inputElements.forEach((ele, index) => {
+    ele.addEventListener("keydown", (e) => {
+      // if the keycode is backspace & the current field is empty
+      // focus the input before the current. The event then happens
+      // which will clear the input before the current
+      if (e.keyCode === 8 && e.target.value === "") {
+        inputElements[Math.max(0, index - 1)].focus();
+      }
+    });
+    ele.addEventListener("input", (e) => {
+      if (e.target.value === "") {
+        inputElements[index].classList = "code-input";
+      } else {
+        inputElements[index].classList = "code-input active";
+      }
+
+      // take the first character of the input
+      // this actually breaks if you input an emoji like 👨‍👩‍👧‍👦....
+      // but I'm willing to overlook insane security code practices.
+      const [first, ...rest] = e.target.value;
+      e.target.value = first ?? ""; // the `??` '' is for the backspace usecase
+      const lastInputBox = index === inputElements.length - 1;
+      const insertedContent = first !== undefined;
+      if (insertedContent && !lastInputBox) {
+        // continue to input the rest of the string
+        inputElements[index + 1].focus();
+        inputElements[index + 1].value = rest.join("");
+        inputElements[index + 1].dispatchEvent(new Event("input"));
+      }
+    });
+  });
+}
+
+function showPassword($) {
+  $(".show-password-button-om").on("click", function (e) {
+    e.preventDefault();
+
+    if ($(this).parent().find("input").attr("type") == "text") {
+      $(this).parent().find("input").attr("type", "password");
+      $(this).removeClass("show-om");
+    } else {
+      $(this).parent().find("input").attr("type", "text");
+      $(this).addClass("show-om");
+    }
+  });
+}
+
+function collapseFooterMenusInSmallScreens($) {
+  if ($(window).width() <= 991) {
+    $(".collapse-head-om").on("click", function (e) {
+      e.preventDefault();
+
+      $(".collapse-head-om")
+        .not(this)
+        .parent()
+        .find(".list-collapse-om")
+        .slideUp();
+      $(this)
+        .parent()
+        .find(".list-collapse-om")
+        .slideToggle({
+          queue: false,
+          complete: function () {
+            $(".list-collapse-om").each(function () {
+              if ($(this).css("display") == "none") {
+                $(this).parent().removeClass("active");
+              } else {
+                $(this).parent().addClass("active");
+              }
+            });
+          },
+        });
+    });
+  }
+}
+
+function toggleSideMenuInSmallScreens($) {
+  // nav men activation
+  $("#menu-butt-activ-om").on("click", function (e) {
+    e.preventDefault();
+
+    $("#navbar-menu-om").addClass("active-menu");
+    $(".overlay").addClass("active");
+    $("body").addClass("overflow-body");
+  });
+
+  // nav men close
+  $(".close-button__ , .overlay ").on("click", function (e) {
+    e.preventDefault();
+    $("#navbar-menu-om").removeClass("active-menu");
+    $(".overlay").removeClass("active");
+
+    $("body").removeClass("overflow-body");
+  });
+}
+
+function stickyHeader($) {
+  let lastScroll = 0;
+  const fixedHeaderElement = $(".fixed_header__");
+
+  $(document).on("scroll", function () {
+    let currentScroll = $(this).scrollTop();
+
+    const isScrollingDown = function () {
+      return currentScroll < lastScroll && currentScroll > 500;
+    };
+    const isScrollingUp = function () {
+      return currentScroll > lastScroll && currentScroll > 500;
+    };
+
+    const fixedHeightToHeaderWrapper = function (fixedHeaderElement) {
+      const fixedHeaderElementHeight = fixedHeaderElement.innerHeight();
+      $(".main_header__").css("height", fixedHeaderElementHeight);
+    };
+
+    fixedHeightToHeaderWrapper(fixedHeaderElement);
+
+    if (isScrollingDown()) {
+      fixedHeaderElement
+        .addClass("active_menu__")
+        .removeClass("not_active_menu__");
+    } else if (isScrollingUp()) {
+      fixedHeaderElement
+        .removeClass("active_menu__")
+        .addClass("not_active_menu__");
+    } else {
+      fixedHeaderElement.removeClass("active_menu__ not_active_menu__");
+    }
+    lastScroll = currentScroll;
+  });
+}
+
+function dragFilesUpload() {
+  const uploadFileInput = document.getElementById("drag_file_upload");
+  const outputFileParent = document.getElementById("uploaded_files");
+  const uploadFileInputLabel = document.getElementById("drag_file_label");
+  let outputFileChildren = "";
+
+  if (!uploadFileInput) {
+    return;
+  }
+
+  if (outputFileChildren == "") {
+    outputFileParent.style.display = "none";
+  }
+
+  uploadFileInput.addEventListener("change", () => {
+    const formatFileSize = function (bytes) {
+      const sufixes = ["B", "kB", "MB", "GB", "TB"];
+      const i = Math.floor(Math.log(bytes) / Math.log(1024));
+      return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${sufixes[i]}`;
+    };
+
+    for (let i = 0; i < uploadFileInput.files.length; i++) {
+      if (i >= 5) {
+        break;
+      }
+      let file = uploadFileInput.files[i];
+      let fileName = file.name;
+      let fileSize = formatFileSize(file.size);
+
+      if (file.type === "image/jpeg" || file.type === "image/png") {
+        outputFileChildren += `
+            <div class="strip">
+              <p class="file_name">
+                ${fileName}
+                <span class="file_size"> حجم الملف ${fileSize}</span>
+              </p>
+              <button class="delete_file_button" onclick="return this.parentNode.remove()">
+                حذف
+              </button>
+            </div>
+          `;
+      }
+    }
+    uploadFileInputLabel.classList.add("change_border");
+    outputFileParent.style.display = "block";
+    outputFileParent.innerHTML = outputFileChildren;
+  });
+}
+
+function changeProductCardShow() {
+  document.querySelectorAll(".cards_view_button").forEach(function (element) {
+    element.addEventListener("click", function () {
+      let productCard = document
+        .querySelector(`.${this.getAttribute("target")}`)
+        .querySelectorAll(".product_card");
+
+        if (this.classList.contains('virtical_cards_view')) {
+          productCard.forEach((element) => {
+            element.classList.add("vertical_view");
+          });
+          this.classList.add('active');
+        } else {
+          productCard.forEach((element) => {
+            element.classList.remove("vertical_view");
+          });
+          this.classList.add('active');
+        }
+
+    });
+  });
+}
